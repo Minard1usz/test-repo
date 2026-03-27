bf0e767c53fe990464b5dc40e9af230cc4a5811a
Yevhen Larin <minard1usz@gmail.com>
2026-03-28 00:56:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class Comment:
+    total_comments = 0
+
+    def __init__(self, text):
+        self.text = text
+        self.vote_qty = 0
+        Comment.total_comments += 1
+
+
+first_comment = Comment("First comment")
+print(Comment.total_comments)
+
+Comment.total_comments = 10
+
+print(Comment.total_comments)
+print(first_comment.total_comments)
